ac98a34a4df996b48b9a3742d6de3410d9ebc525
tomer7 <tomer66@icloud.com>
2023-01-11 16:07:42 +0200

    Fix reducer product state

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,17 +1,18 @@
 const notFound = (req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`)
-  res.status(404)
-  next(error)
+   const error = new Error(`Not Found - ${req.originalUrl}`)
+   res.status(404)
+   next(error)
 }
 
 // custom middleware for error
+// overwrite the default error handler
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-  res.status(statusCode)
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-  })
+   const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+   res.status(statusCode)
+   res.json({
+      message: err.message,
+      stack: process.env.NODE_ENV === 'production' ? null : err.stack
+   })
 }
 
 export { notFound, errorHandler }
